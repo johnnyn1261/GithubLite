1f9723a0e4c747b6f068594213f646a9da18954c
johnnyn1261 <johnnyn1261@gmail.com>
2020-03-25 01:03:31 -0400

    Updates

---
+++ precache-manifest.2649ce3d44a6e3b3959ca157345d62d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2335af0cbbf8c35681aa22062b289a",
+    "url": "/GithubLite/index.html"
+  },
+  {
+    "revision": "b7ff8dd547f8614d1c32",
+    "url": "/GithubLite/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "b85d15cddd52bf59c2e5",
+    "url": "/GithubLite/static/js/2.4c3a79f7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/GithubLite/static/js/2.4c3a79f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ff8dd547f8614d1c32",
+    "url": "/GithubLite/static/js/main.32e8e600.chunk.js"
+  },
+  {
+    "revision": "b47219276ad54df06a11",
+    "url": "/GithubLite/static/js/runtime-main.838b2710.js"
+  }
+]);
