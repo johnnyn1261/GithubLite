97bffb3173e4e77d56bece260f388807116f0713
johnnyn1261 <johnnyn1261@gmail.com>
2020-03-30 00:34:16 -0400

    Added search functionality and null checking

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   async componentDidMount() {
@@ -23,17 +25,44 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
-  searchGitUsers = text => {
+  // Search GitHub for users
+  searchGitUsers = async text => {
+    this.setState({ loading: true });
     console.log(text);
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&
+      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearGitUsers = () => this.setState({ users: [], loading: false });
+
+  // Set alert for error
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
   };
 
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchGitUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert}></Alert>
+          <Search
+            searchUsers={this.searchGitUsers}
+            clearUsers={this.clearGitUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
